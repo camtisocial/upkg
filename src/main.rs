2472fcd81690f15cc2b3c8b87fedd0c296f13d9d
camtisocial <thompsonca99@gmail.com>
2025-12-15 14:51:35 -0800

    added command line arg for local vs sync mode

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,16 @@ mod managers;
 mod ui;
 
 fn main() {
+    //checking for flags
+    let args: Vec<String> = std::env::args().collect();
+    let local_mode = args.contains(&"--local".to_string()) || args.contains(&"-l".to_string());
+
+    if local_mode {
+        println!("[LOCAL]");
+    } else {
+        println!("[SYNC]");
+    }
+
+    println!();
     ui::draw_ui();
 }
